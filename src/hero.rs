0d1090b966118411285b07b3c146345963c893f9
Swapnil Sharma <nilinswap@gmail.com>
2021-11-07 22:05:58 +0530

    fixed build errors: test all apis, it works

--- src/hero.rs
+++ src/hero.rs
@@ -2,11 +2,38 @@ use diesel;
 use diesel::prelude::*;
 use crate::schema::heroes;
 
+use diesel::pg::PgConnection;
+
+
 #[table_name = "heroes"]
-#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Serialize, Deserialize, AsChangeset, Queryable, Insertable)]
 pub struct Hero {
     pub id: Option<i32>,
     pub firstname: String,
     pub lastname: String,
     pub phone: String,
+}
+
+
+impl Hero {
+    pub fn create(hero: Hero, connection: &PgConnection) -> Hero {
+        diesel::insert_into(heroes::table)
+            .values(&hero)
+            .execute(connection)
+            .expect("Error creating new hero");
+
+        heroes::table.order(heroes::id.desc()).first(connection).unwrap()
+    }
+
+    pub fn read(connection: &PgConnection) -> Vec<Hero> {
+        heroes::table.order(heroes::id.asc()).load::<Hero>(connection).unwrap()
+    }
+
+    pub fn update(id: i32, hero: Hero, connection: &PgConnection) -> bool {
+        diesel::update(heroes::table.find(id)).set(&hero).execute(connection).is_ok()
+    }
+
+    pub fn delete(id: i32, connection: &PgConnection) -> bool {
+        diesel::delete(heroes::table.find(id)).execute(connection).is_ok()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,31 @@ mod hero;
 use hero::{Hero};
 
 #[post("/", data = "<hero>")]
-fn create(hero: Json<Hero>) -> Json<Hero> {
-    hero
+fn create(hero: Json<Hero>, connection: db::Connection) -> Json<Hero> {
+    let insert = Hero { id: None, ..hero.into_inner() };
+    Json(Hero::create(insert, &connection))
 }
 
 #[get("/")]
-fn read() -> Json<JsonValue>  {
-    println!("pring");
-    Json(json!([
-        "hero 1", 
-        "hero 2"
-    ]))
+fn read(connection: db::Connection) -> Json<JsonValue> {
+    Json(json!(Hero::read(&connection)))
 }
 
 #[put("/<id>", data = "<hero>")]
-fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
-    hero
+fn update(id: i32, hero: Json<Hero>, connection: db::Connection) -> Json<JsonValue> {
+    let update = Hero { id: Some(id), ..hero.into_inner() };
+    Json(json!({
+        "success": Hero::update(id, update, &connection)
+    }))
 }
 
 #[delete("/<id>")]
-fn delete(id: i32) -> Json<JsonValue> {
-    Json(json!({"status": "ok"}))
+fn delete(id: i32, connection: db::Connection) -> Json<JsonValue> {
+    Json(json!({
+        "success": Hero::delete(id, &connection)
+    }))
 }
+
 
 fn main() {
     rocket::ignite()
