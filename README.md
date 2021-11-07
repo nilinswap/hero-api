644a70645b31c14883a0f3660081ffb0b398b00b
Swapnil Sharma <nilinswap@gmail.com>
2021-11-07 20:18:56 +0530

    without persistence

---
+++ README.md
@@ -0,0 +1,13 @@
+#README
+
+[main tutorial follow](https://medium.com/sean3z/building-a-restful-crud-api-with-rust-1867308352d8)
+
+[code for main.rs to solve plugin error](https://rocket.rs/v0.4/guide/getting-started/#hello-world)
+
+use latest rocket see cargo toml
+
+[json is no more the way it is defined in here](https://stackoverflow.com/questions/61106971/i-get-a-no-json-in-root-error-when-building-a-rust-rocket-api)
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::Json;
+use rocket_contrib::json::JsonValue;
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn read() -> Json<JsonValue>  {
+    println!("pring");
+    Json(json!([
+        "hero 1", 
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<JsonValue> {
+    Json(json!({"status": "ok"}))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
+        .launch();
 }
